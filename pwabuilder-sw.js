a094ca6fd369633dab2dc3307d297abaf6a83b19
BomiDo <bm.do.bomhada@gmail.com>
2021-12-25 14:41:39 +0900

    EDIT

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,48 +1,43 @@
-// This is the "Offline page" service worker
+// 캐시 이름
+const CACHE_NAME = "shopping-v1";
 
-importScripts(
-  'https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js'
-);
+// 캐싱할 파일
+const FILES_TO_CACHE = [
+    "offline.html",
+    "favicon.ico"
+];
 
-const CACHE = 'pwabuilder-page';
-
-const offlineFallbackPage = 'offline.html';
-
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE).then((cache) => cache.add(offlineFallbackPage))
-  );
-});
-
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      (async () => {
-        try {
-          const preloadResp = await event.preloadResponse;
-
-          if (preloadResp) {
-            return preloadResp;
-          }
-
-          const networkResp = await fetch(event.request);
-          return networkResp;
-        } catch (error) {
-          const cache = await caches.open(CACHE);
-          const cachedResp = await cache.match(offlineFallbackPage);
-          return cachedResp;
-        }
-      })()
+// 상술한 파일 캐싱
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
     );
-  }
+});
+
+// CACHE_NAME이 변경되면 오래된 캐시 삭제
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+      caches.keys().then((keyList) =>
+        Promise.all(
+          keyList.map((key) => {
+            if (CACHE_NAME !== key)    
+              return caches.delete(key);
+          })
+        )
+      )
+    );
+});
+
+// 요청에 실패하면 오프라인 페이지 표시
+self.addEventListener("fetch", (event) => {
+  if ("navigate" !== event.request.mode)  
+    return;
+
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches
+        .open(CACHE_NAME)
+        .then((cache) => cache.match("offline.html"))
+    )
+  );
 });
