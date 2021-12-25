3dd0da761e4bad0eb46ba08cd3519f9413c25f36
BomiDo <bm.do.bomhada@gmail.com>
2021-12-25 19:26:25 +0900

    EDIT

--- service-worker.js
+++ service-worker.js
@@ -1,48 +1,28 @@
-// This is the "Offline page" service worker
 
-importScripts(
-  'https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js'
-);
+const CACHE_NAME = 'pwabuilder-page-v1';
 
-const CACHE = 'pwabuilder-page';
+// CODELAB : Add list of files to cache here.
+const FILES_TO_CACHE = '/offline.html';
 
-const offlineFallbackPage = 'offline.html';
-
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE).then((cache) => cache.add(offlineFallbackPage))
+self.addEventListener('install', evt => {
+  // CODELAB : Precache static resources here.
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log(`[ServiceWorker] Pre-caching offline page`);
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
 });
 
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      (async () => {
-        try {
-          const preloadResp = await event.preloadResponse;
-
-          if (preloadResp) {
-            return preloadResp;
-          }
-
-          const networkResp = await fetch(event.request);
-          return networkResp;
-        } catch (error) {
-          const cache = await caches.open(CACHE);
-          const cachedResp = await cache.match(offlineFallbackPage);
-          return cachedResp;
+self.addEventListener('activate', evt => {
+  // CODELAB : Remove previous cached data from disk.
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME) {
+          console.log('[SerciveWorker] Removing old cache', key);
         }
-      })()
-    );
-  }
+      }));
+    })
+  );
 });
